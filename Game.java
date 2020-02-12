69576c54f092568bcbdc44cfeb9060ef5d3c49b2
Jean <jmkd@yahoo.com>
2020-02-12 10:50:52 +0100

    implementation du methode goRoom

--- Game.java
+++ Game.java
@@ -161,41 +161,20 @@ public class Game
             System.out.println("Go where?");
             return;
         }
-
-        String direction = command.getSecondWord();
-/* 
         // Try to leave current room.
+        String direction = command.getSecondWord();
         Room nextRoom = null;
-        if(direction.equals("north")) {
-            nextRoom = currentRoom.northExit;
-        }
-        if(direction.equals("east")) {
-            nextRoom = currentRoom.eastExit;
-        }
-        if(direction.equals("south")) {
-            nextRoom = currentRoom.southExit;
-        }
-        if(direction.equals("west")) {
-            nextRoom = currentRoom.westExit;
-        }
+        currentRoom.setExits(direction, currentRoom);
 
         if (nextRoom == null) {
             System.out.println("There is no door!");
         }
         else {
             currentRoom = nextRoom;
-            System.out.println("You are " + currentRoom.getDescription());
-            System.out.print("Exits: ");
-            if(currentRoom.northExit != null)
-                System.out.print("north ");
-            if(currentRoom.eastExit != null)
-                System.out.print("east ");
-            if(currentRoom.southExit != null)
-                System.out.print("south ");
-            if(currentRoom.westExit != null)
-                System.out.print("west ");
-            System.out.println();
-        } */  
+            printLocationInfo();
+        }
+        
+     
     }
     private void printLocationInfo(){
         System.out.println("You are " + currentRoom.getDescription());
